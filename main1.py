fd77223429b96c9471fc2ba8943c1b23d095736f
Avinash Lodhi <avinashlodhi58@gmail.com>
2025-07-04 01:08:11 +0530

    Updated main1.py

--- main1.py
+++ main1.py
@@ -5,8 +5,8 @@ import streamlit as st
 st.title("🎓Placement Prediction Model....")
 model=joblib.load("model1.pkl")
 scaler=joblib.load("scaler1.pkl")
-CGPA = st.number_input("Enter CGPA 0-10")
-IQ = st.number_input("Enter IQ 0-200")
+CGPA = st.number_input("Enter CGPA (0-10)")
+IQ = st.number_input("Enter IQ (0-200)")
 
 if st.button("Predict Placement"):
     data=pd.DataFrame([[CGPA,IQ]],columns=["cgpa",'iq'])
